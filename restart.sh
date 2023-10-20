ea9deefc2e9e9539fa1ba9c0430b51dc1c1963fe
liuyuchan <liuyuchan@gmail.com>
2023-10-21 07:08:06 +0800

    init

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
-admin="sweat_x"
+admin="card_server"
 
 #清空文件夹
-#rm -rf ${admin}
+rm -rf ${admin}
 
-#docker run -i --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone -b hp_online https://chenweilong961022:ch1433471850@gitee.com/chenweilong961022/${admin}.git
+docker run -i --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://chenweilong961022:ch1433471850@gitee.com/chenweilong961022/${admin}.git
 
 docker run -i --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -v maven-repo:/root/.m2 -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install -Dmaven.test.skip=true -f ${admin}/admin/pom.xml
 
