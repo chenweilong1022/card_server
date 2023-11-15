6e1103d44d6f96017b1d0e589a4e35daf2dd684e
liuyuchan286 <liuyuchan286@gmail.com>
2023-11-15 09:05:08 +0700

    1

--- admin_vue/build.sh
+++ admin_vue/build.sh
@@ -1,5 +1,5 @@
 rm -rf /Users/chenweilong/Desktop/java代码/card_server/admin/src/main/resources/static/*
 npm run build
-cp -r /Users/chenweilong/Desktop/java代码/card_server/admin_vue/dist/* D:/Users/Admin/IdeaProjects/card_server/admin/src/main/resources/static/
+cp -r /Users/chenweilong/Desktop/java代码/card_server/admin_vue/dist/* /Users/chenweilong/Desktop/java代码/card_server/admin/src/main/resources/static/
 rm -rf ./dist/
 read name
