ce675a82d3403b6df8fe47b492e3b5b28feca3c0
liuyuchan286 <liuyuchan286@gmail.com>
2024-01-09 00:28:02 +0800

    11

--- admin/src/main/resources/static/config/index.js
+++ admin/src/main/resources/static/config/index.js
@@ -13,7 +13,7 @@
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '2401081605';   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '2401090027';   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
 
